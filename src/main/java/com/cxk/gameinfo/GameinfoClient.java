eda5f2d050e7497fc9b577d7234ce069c6fb326a
HouYunFei <1844025705@qq.com>
2025-06-20 23:31:54 +0800

    feat(keybind): 添加游戏信息 GUI 的快捷键绑定
    - 新增 KeybindHandler 类，用于注册快捷键绑定
    - 在 GameinfoClient 中集成 KeybindHandler
    - 添加英文和中文语言文件，定义快捷键和类别名称
    - 更新 mod 版本号至 1.4

--- src/main/java/com/cxk/gameinfo/GameinfoClient.java
+++ src/main/java/com/cxk/gameinfo/GameinfoClient.java
@@ -3,6 +3,7 @@ package com.cxk.gameinfo;
 import com.cxk.gameinfo.command.GameInfoCommand;
 import com.cxk.gameinfo.config.GameInfoConfig;
 import com.cxk.gameinfo.hud.HudOverlay;
+import com.cxk.gameinfo.keybind.KeybindHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
@@ -19,5 +20,6 @@ public class GameinfoClient implements ClientModInitializer {
         HudOverlay hudOverlay = new HudOverlay();
         HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, Identifier.of("gameinfo", "custom_text"), hudOverlay); // 注册HUD元素
         CommandRegistrationCallback.EVENT.register(GameInfoCommand::register);
+        KeybindHandler.register(); // 注册按键绑定
     }
 }
---
+++ src/main/java/com/cxk/gameinfo/keybind/KeybindHandler.java
@@ -0,0 +1,31 @@
+package com.cxk.gameinfo.keybind;
+
+import com.cxk.gameinfo.gui.GameInfoConfigScreen;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class KeybindHandler {
+    private static KeyBinding openGuiKeyBinding;
+
+    public static void register() {
+        openGuiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.gameinfo.open_gui", // 翻译键
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_P, // P键
+                "category.gameinfo" // 类别
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiKeyBinding.wasPressed()) {
+                MinecraftClient mc = MinecraftClient.getInstance();
+                if (mc.currentScreen == null) {
+                    mc.setScreen(new GameInfoConfigScreen(null));
+                }
+            }
+        });
+    }
+} 
